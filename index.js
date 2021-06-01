394e1d9239a271ef0af48c909b8c13d70837522c
stacysorey <stacysorey@gmail.com>
2021-05-31 20:18:57 -0400

    commit tree 244f88c4dd622df377fd1cc95d0afccb4bfa23a7

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+// Returns Good Morning before 12pm
+// Returns Good Afternoon between 12pm and 5pm 
+// Returns Good Evening after 5pm
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+
+// updates the DOM with an appropriate message
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
